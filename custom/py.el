e552b6ea210940bd171ad3b6adc753fd3c9ac35f
Doug MacEachern <dougm@vmware.com>
2013-03-06 15:26:27 -0800

    rename pychecker -> pyflymake

--- custom/py.el
+++ custom/py.el
@@ -7,7 +7,7 @@
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list "pychecker" (list local-file))))
+      (list "pyflymake" (list local-file))))
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pyflakes-init)))
 (add-hook 'python-mode-hook 'flymake-mode)
