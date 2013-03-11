aa867c8e26da43d37369e9f58586fba6f0c51194
Doug MacEachern <dougm@vmware.com>
2013-03-11 09:26:13 -0700

    2 space indent for .thrift

--- custom/thrift.el
+++ custom/thrift.el
@@ -1,3 +1,4 @@
 (autoload 'thrift-mode "thrift-mode" "Major mode for editing Thrift code." t)
 (add-to-list 'auto-mode-alist '("\.thrift$" . thrift-mode))
-
+(add-hook 'thrift-mode-hook (lambda ()
+            (setq thrift-indent-level 2)))
