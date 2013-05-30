8133ebc3db9d93028d3676f7715b00dba6935c82
Doug MacEachern <dougm@vmware.com>
2013-05-30 10:33:45 -0700

    fix .pp auto-mode-alist

--- custom/pp.el
+++ custom/pp.el
@@ -1,3 +1,3 @@
 (autoload 'puppet-mode "puppet-mode" "Major mode for editing Puppet code." t)
-(add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode))
+(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
 (add-hook 'puppet-mode-hook 'esk-prog-mode-hook)
