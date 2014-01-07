b9c68338d7289eba2e41eb3f293d6dc2eb520091
Doug MacEachern <dougm@vmware.com>
2014-01-07 15:33:42 -0800

    go-test improvements

--- custom/go.el
+++ custom/go.el
@@ -9,12 +9,6 @@
           go-lint
           go-mode))
 
-;;; go-mode adds this to the end of the list, but doesn't work as advertised
-(require 'compile)
-(add-to-list 'compilation-error-regexp-alist 'gotest)
-(add-to-list 'compilation-error-regexp-alist-alist
-             '(gotest . ("^\t+\\([^()\t\n]+\\):\\([0-9]+\\):? .*$" 1 2)))
-
 ;;; ignore 'go test -c' files
 (push ".test" completion-ignored-extensions)
 
@@ -36,39 +30,51 @@
   (interactive)
   (compile "go build"))
 
-(defun go-test-all ()
-  "test project"
-  (interactive)
-    (compile "go test -v"))
-
-(defun go-test-one ()
-  "run a single test, closest to current point"
-  (interactive)
-    (compile (concat "go test -v -test.run " (go-current-test))))
-
-(defun go-test-module ()
-  "test module"
-  (interactive)
-    (compile (concat "go test -v -test.run '" (go-grep-tests) "'")))
-
-(defun go-check ()
-  "gocheck project"
-  (interactive)
-    (compile "go test -gocheck.vv"))
-
 (defun go-run ()
   "go run current buffer"
   (interactive)
     (compile (concat "go run " buffer-file-name)))
 
-(defun go-current-test ()
+(defun go-test-all ()
+  "test project"
+  (interactive)
+    (go-test--run "go test -v"))
+
+(defun go-test-one ()
+  "run a single test, closest to current point"
+  (interactive)
+    (go-test--run (concat "go test -v -test.run " (go-test--current))))
+
+(defun go-test-module ()
+  "test module"
+  (interactive)
+    (go-test--run (concat "go test -v -test.run '" (go-test--find) "'")))
+
+;;; run 'go test ...' with our compile hook
+(defun go-test--run (cmd)
+  (add-hook 'compilation-start-hook 'go-test--compilation-hook)
+  (compile cmd)
+  (remove-hook 'compilation-start-hook 'go-test--compilation-hook))
+
+;;; make local so we only match go test errors
+(defun go-test--compilation-hook (p)
+  (set (make-local-variable 'compilation-error-regexp-alist)
+       '(("^\t+\\([^()\t\n]+\\):\\([0-9]+\\):? .*$" 1 2))))
+
+;;; generate regex for all Tests in the current buffer
+;;; note that go test has a '-file' flag that does not work as expected
+(defun go-test--find ()
+  (save-excursion
+    (goto-char (point-min))
+    (let (res)
+      (while (re-search-forward "^func \\(Test[a-zA-Z0-9_]+\\)" nil t)
+        (let ((test (buffer-substring-no-properties (match-beginning 1) (match-end 1))))
+          (setq res (if res (concat res "|" test) test))))
+      res)))
+
+;;; find test closest to current point
+(defun go-test--current ()
   (save-excursion
     (re-search-backward
      "^func \\(Test[a-zA-Z0-9_]+\\)" nil t)
     (buffer-substring-no-properties (match-beginning 1) (match-end 1))))
-
-;;; generate regex for all Tests in the current buffer
-;;; note that go test has a '-file' flag that does not work as expected
-(defun go-grep-tests ()
-  (car (process-lines "bash" "-c"
-                      (concat "egrep -o 'func Test[^(]+' " buffer-file-name " | awk '{print $2}' | paste -sd '|'"))))
