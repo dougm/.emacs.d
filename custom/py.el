2bb9603ed6476a636736053815ff4a450c87959e
Doug MacEachern <dougm@vmware.com>
2014-01-24 15:46:40 -0800

    python tweaks

--- custom/py.el
+++ custom/py.el
@@ -15,24 +15,25 @@
       (list "pyflymake" (list local-file))))
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pyflakes-init)))
+
 (add-hook 'python-mode-hook 'flymake-mode)
 
 (add-to-list 'auto-mode-alist '("\\.sc$" . python-mode))
 
 (add-hook 'python-mode-hook
-(lambda ()
-  (run-hooks 'prog-mode-hook)
-  (local-set-key (kbd "RET") 'py-newline-and-indent)
-  (local-set-key (kbd "C-c a") 'nosetests-all)
-  (local-set-key (kbd "C-c m") 'nosetests-module)
-  (local-set-key (kbd "C-c .") 'nosetests-one)
-))
-
-(setq jedi:key-related-names (kbd "C-c l"))
-(setq jedi:key-show-doc (kbd "C-c d"))
-(setq jedi:key-goto-definition (kbd "C-c o"))
-(setq jedi:goto-definition-pop-marker (kbd "C-c -"))
-
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:setup-keys t)
-(setq jedi:complete-on-dot t)
+          (lambda ()
+            (run-hooks 'prog-mode-hook)
+            (setq jedi:complete-on-dot t)
+            (jedi:setup)
+            (let ((map python-mode-map))
+              (define-key map [(meta i)] 'py-indent-line)
+              (define-key map (kbd "TAB") 'py-indent-line-outmost))
+            (let ((map jedi-mode-map))
+              (define-key map (kbd "C-c .") nil)
+              (define-key map (kbd "C-c d") 'jedi:show-doc)
+              (define-key map (kbd "C-c o") 'jedi:goto-definition)
+              (define-key map (kbd "C-c -") 'jedi:goto-definition-pop-marker))
+            (local-set-key (kbd "RET") 'py-newline-and-indent)
+            (local-set-key (kbd "C-c a") 'nosetests-all)
+            (local-set-key (kbd "C-c m") 'nosetests-module)
+            (local-set-key (kbd "C-c .") 'nosetests-one)))
