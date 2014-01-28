7b38c33d682cb84f1dd4c730cfcc5de9203203e8
Doug MacEachern <dougm@vmware.com>
2014-01-28 15:46:09 -0800

    Vagrantfile ruby-mode

--- custom/rb.el
+++ custom/rb.el
@@ -5,3 +5,4 @@
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 ;; wtf python-mode has: (defvar ruby-indent-level nil)
 (setq ruby-indent-level 2)
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
