1e9c844b79991b9018a8ae3c051162e11c7d3135
Doug MacEachern <dougm@vmware.com>
2014-01-31 17:33:18 -0800

    vagrant-tramp-term key binding

--- custom/term.el
+++ custom/term.el
@@ -13,12 +13,15 @@
             (define-key term-raw-map (kbd "C-'") 'term-line-mode)
             (define-key term-mode-map (kbd "C-'") 'term-char-mode)
             (define-key term-raw-map (kbd "C-y") 'term-paste)
+            (term-set-escape-char ?\C-x)
             ))
 
 (autoload 'multi-term "multi-term" nil t)
 (autoload 'multi-term-next "multi-term" nil t)
+(autoload 'vagrant-tramp-term "vagrant-tramp")
 
 (setq multi-term-program "bash")
 
 (global-set-key (kbd "C-c t") 'multi-term-next)
 (global-set-key (kbd "C-c T") 'multi-term)
+(global-set-key (kbd "C-c v") 'vagrant-tramp-term)
