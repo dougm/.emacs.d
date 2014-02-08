5bbb53eae5e7037cbab6bd1e00c1c7ba763db072
Doug MacEachern <dougm@vmware.com>
2014-02-07 21:37:40 -0800

    recompile .elc on-save

--- custom/el.el
+++ custom/el.el
@@ -6,9 +6,19 @@
         (find-variable-other-window symb)
       (find-function-at-point))))
 
+(defun recompile-elc-on-save ()
+  "Recompile your elc when saving an elisp file."
+  (add-hook 'after-save-hook
+            (lambda ()
+              (when (file-exists-p (byte-compile-dest-file buffer-file-name))
+                (emacs-lisp-byte-compile)))
+            nil
+            t))
+
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
-            (turn-on-eldoc-mode)
+            (eldoc-mode t)
+            (recompile-elc-on-save)
             (local-set-key (kbd "C-h C-f") 'find-function-at-point)
             (local-set-key (kbd "C-h C-v") 'find-variable-at-point)
             (local-set-key (kbd "C-c o") 'find-symbol-at-point)
